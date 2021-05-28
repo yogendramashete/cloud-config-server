1511493a4cbe772c0a55f732ca1a852fca058319
yogendramashete <yogendra.mashete@gmail.com>
2021-05-28 15:08:59 +0530

    Initial commit

---
+++ src/main/kotlin/poc/yogendra/cloudconfigserver/CloudConfigServerApplication.kt
@@ -0,0 +1,15 @@
+package poc.yogendra.cloudconfigserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+class CloudConfigServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<CloudConfigServerApplication>(*args)
+}
